e3775e687662952c6de4adf789ee8941618b23e7
your_name <your_email>
2014-08-07 21:58:07 +0800

    finish the function of showing the error on  sign_up page

--- huodongtong/app/models/user.rb
+++ huodongtong/app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
   before_create{ generate_token(:token)}
-
-
-  validates :name, presence: true,uniqueness: {:case_sensitive => false}
-  validates :password,length: { minimum: 6 }
-  validates :forget_password_question,presence: true
+  validates :name, :presence=> true,uniqueness: {:case_sensitive => false}
+  validates :password,:presence => true, :on => :create
   has_secure_password
+  validates :forget_password_question,presence: true
   validates :forget_password_answer,presence: true
   def generate_token(column)
     begin
