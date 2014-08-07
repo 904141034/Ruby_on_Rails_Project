d893e2efb33f043f36c421d639e2eaab9dc5ef1f
your_name <your_email>
2014-08-07 11:17:49 +0800

    finish the signup function for the huodongtong first card

--- huodongtong/app/controllers/users_controller.rb
+++ huodongtong/app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+#encoding:utf-8
 class UsersController < ApplicationController
   def welcome
   end
@@ -7,5 +8,18 @@ class UsersController < ApplicationController
   end
 
   def login
+  end
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      redirect_to :root,:notice=>"注册成功"
+    else
+      render :signup
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:password,:password_confirmation,:forget_password_question,:forget_password_answer)
   end
 end
